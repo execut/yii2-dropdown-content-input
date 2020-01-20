f116bc84f93d4d3cc63ec1838fa9e267e6f7d6e3
eXeCUT <sexecut@gmail.com>
2020-01-20 09:46:49 +0300

    hack for disable js error

--- assets/DropdownContent.js
+++ assets/DropdownContent.js
@@ -199,7 +199,7 @@ $.widget("execut.dropdownContent", {
     },
     _recalcContainerPosition: function () {
         var t = this;
-        if (!t.element.hasClass('active') || !t.containerEl.length || !t.containerEl[0].ownerDocument) {
+        if (!t.element.hasClass('active') || !t.containerEl.length || !t.containerEl[0].ownerDocument || !t.containerEl.parent().length || !t.containerEl.parent()[0].ownerDocument) {
             return;
         }
 
